3b9999a4242496c19eac97351159d868ef00e034
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-01-23 10:48:09 +0100

    Add DogsList.js with basic text

---
+++ src/components/DogsList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class DogsList extends Component {
+    render() {
+        return (
+            <div className="dogs-list">
+                <h1>Dogs List</h1>
+            </div>
+        )
+    }
+}
+
+export default DogsList
