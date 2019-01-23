8193aa9b3e14c7bf9103c2c01c4c1d586c4c12b7
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-01-23 12:00:57 +0100

    Seperate concerns by splitting component into two components

--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react'
 import * as request from 'superagent'
+import DogsListContainer from './DogsListContainer'
 
-class DogsList extends Component {
-    state = { dogBreeds: null }
-
-    componentDidMount() {
-        request
-            .get('https://dog.ceo/api/breeds/list/all')
-            .then(response => this.updateBreeds(Object.keys(response.body.message)))
-            .catch(console.error)
-    }
-
-    updateBreeds(breeds) {
-        this.setState({
-            dogBreeds: breeds
-        })
-    }
-
+class DogsList extends Component {    
     render() {
+        const { dogBreeds } = this.props
         return (
             <div className="dogs-list">
                 <h1>Dogs List</h1>
-                {this.state.dogBreeds === null ? 'Loading...' :   <ul>
-                    { this.state.dogBreeds.map(breed => <li key={breed}>{breed}</li>)}
+                {dogBreeds === null ? 'Loading...' : <ul>
+                    {dogBreeds.map(breed => <li key={breed}>{breed}</li>)}
                 </ul>}
-               
-
             </div>
         )
     }
